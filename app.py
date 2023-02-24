93f84ace58970d68dbb32c8eab2769b49a14465b
Darth Vader <danieltarancon@gmail.com>
2023-02-24 17:41:36 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+import string
+
+
+class Caesar_Cipher():
+
+    british_alphabet = string.ascii_uppercase
+
+    def __init__(self, key):
+        self.key = key
+
+    def encrypt(self):
+        pass
+    def decrypt(self):
+        pass
+
+
+
+
+
