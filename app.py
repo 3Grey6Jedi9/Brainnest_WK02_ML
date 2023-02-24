6795541791e7190ccb70a3be6e3568bf98c5160c
Darth Vader <danieltarancon@gmail.com>
2023-02-24 18:09:31 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,19 +4,40 @@ import datetime
 
 class Caesar_Cipher():
 
-    british_alphabet = string.ascii_uppercase
+    british_alphabet = list(string.ascii_uppercase)
 
     def __init__(self, key):
         self.key = key
 
     def host(self):
         current_time = datetime.datetime.now()
-    def encrypt(self):
-        pass
+        moment = ''
+        if  6 <= current_time.hour and current_time.hour < 12:
+            moment = 'morning'
+        elif 12 <= current_time.hour and current_time.hour < 18:
+            moment = 'afternoon'
+        elif 18 <= current_time.hour and current_time.hour < 22:
+            moment = 'evening'
+        elif  22 <= current_time.hour and current_time.hour < 6:
+            moment = 'night'
+        return f'''Good {moment}, I will help you encrypting or decrypting the message you desire'''
+    def encrypt(self, word):
+        global british_alphabet
+        ciphertext = []
+        for w in word.upper():
+            for l in british_alphabet:
+                
+
+
+
     def decrypt(self):
         pass
+    def proceed(self):
+        pass
 
 
-print(datetime.datetime.now())
+encryptor = Caesar_Cipher(5)
+
+print(Caesar_Cipher.british_alphabet)
 
 
